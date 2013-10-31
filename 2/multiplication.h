de911760e9aae3f1d1c924393bb07b2d8fe05c9c
Garnel Mao <maogm12@gmail.com>
2013-11-01 01:35:07 +0800

    init

---
+++ 2/multiplication.h
@@ -0,0 +1,22 @@
+#ifndef _MULTIPLICATION_H_
+#define _MULTIPLICATION_H_
+
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+string GraduateSchool(const string fac1, const string fac2)
+{
+    string rfac1, rfac2;
+    rfac1.assign(fac1.rbegin(), fac1.rend());
+    rfac2.assign(fac2.rbegin(), fac2.rend());
+    string result="";
+
+    for (int i = 0; i < rfac2.size(); ++i) {
+        
+    }
+
+}
+
+#endif
